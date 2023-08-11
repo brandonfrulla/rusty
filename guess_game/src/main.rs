8db3961fa135bcc96be2d46ad4e75722734e66a7
brandonfrulla <bfrulla@mail.umw.edu>
2023-08-11 07:37:26 -0500

    added rand and tweaked existing slightly

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -1,7 +1,10 @@
-use std::io;
+use std::io::{self, Write};
+use rand::Rng;
 
 fn main() {
     let mut guess = String::new();
+
+    let secret : i8 = rand::thread_rng().gen_range(1..=100);
 
     print!("Guessing game initialized. \nEnter a guess: ");
     io::stdout().flush().expect("Toilet's clogged! Failed to flush");
@@ -10,5 +13,7 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println!("You guessed: {guess}")
+    println!("You guessed: {guess}");
+
+    println!("The secret number was: {secret}");
 }
