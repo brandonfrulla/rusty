a330a8e9344be923d6572e364dd82d07f4c0d3f6
brandonfrulla <bfrulla@mail.umw.edu>
2023-08-15 06:51:16 -0500

    finished guessing game, tested a few extra things

--- guess_game/src/main.rs
+++ guess_game/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     println!("Guessing game initialized.");
 
     loop {
+
         let mut guess = String::new();
 
         let secret: u32 = rand::thread_rng().gen_range(1..=100);
@@ -19,19 +20,29 @@ fn main() {
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        println!("You guessed: {}", guess.trim());
+        // println!("You guessed: {}", guess.trim());
 
-        println!("The secret number was: {}", secret);
+        println!("The secret number was: {}", secret); 
 
-        let x: u32 = guess
-            .trim()
-            .parse()
-            .expect("Failed to parse u32 int from input, be sure to type a number.");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Enter a number between 1-100, dummy.");
+                continue;
+            }
+        };
 
-        match x.cmp(&secret) {
-            Ordering::Less => println!("Aim higher."),
-            Ordering::Greater => println!("Way off."),
-            Ordering::Equal => println!("Lucky guess."),
+        // if guess == "quit" { //expected `u32`, found `&str`
+        //     break;
+        // }
+
+        match guess.cmp(&secret) {
+            Ordering::Less => println!("Aim higher.\n"),
+            Ordering::Greater => println!("Way off.\n"),
+            Ordering::Equal => {
+                println!("Lucky guess!\n");
+                break;
+            }
         }
     }
 }
