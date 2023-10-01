408cdabbd15093adac3e7d7942f7f997b0f96cf7
brandonfrulla <bfrulla@mail.umw.edu>
2023-10-01 05:53:06 -0500

    cleanup repo

---
+++ tutorial1/target/debug/deps/tutorial1-e9143da4452f0518.d
@@ -0,0 +1,5 @@
+/Users/frxllx/Desktop/workshop/rusty/tutorial1/target/debug/deps/tutorial1-e9143da4452f0518.rmeta: src/main.rs
+
+/Users/frxllx/Desktop/workshop/rusty/tutorial1/target/debug/deps/tutorial1-e9143da4452f0518.d: src/main.rs
+
+src/main.rs:
