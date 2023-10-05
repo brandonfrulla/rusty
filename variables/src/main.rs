04e1b2e3066656a70381e7340ca0602eac3f338f
brandonfrulla <bfrulla@mail.umw.edu>
2023-10-05 06:24:46 -0500

    Rust book ch3

---
+++ variables/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let x = 7;
+    println!("The value in X is {}", x);
+    x = 8;
+    println!("The value in X is {}", x);
+}
---
+++ variables/target/debug/deps/vars-c65aee980baec3ed.d
@@ -0,0 +1,5 @@
+/Users/frxllx/Desktop/workshop/rusty/variables/target/debug/deps/vars-c65aee980baec3ed: src/main.rs
+
+/Users/frxllx/Desktop/workshop/rusty/variables/target/debug/deps/vars-c65aee980baec3ed.d: src/main.rs
+
+src/main.rs:
