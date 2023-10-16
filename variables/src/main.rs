91f24e75bbb249e56fc36491d39cb2d77a5e6a23
brandonfrulla <bfrulla@mail.umw.edu>
2023-10-16 07:23:25 -0500

    Updating mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-    let x = 7;
-    println!("The value in X is {}", x);
-    x = 8;
-    println!("The value in X is {}", x);
+    let mut x = 5;
+
+    x = x + 1;
+
+    {
+        x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+
+    println!("The value of x is: {x}");
 }
