0d4fd07606dd746d4c2ff243c860c8921082de2b
brandonfrulla <bfrulla@mail.umw.edu>
2023-10-17 06:50:30 -0500

    Tuple study

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -9,4 +9,12 @@ fn main() {
     }
 
     println!("The value of x is: {x}");
+
+    
+    let tup = (500, 6.4, 1);
+    
+    let (_x, y, _z) = tup;
+    
+    println!("The value of y is: {y}");
+
 }
